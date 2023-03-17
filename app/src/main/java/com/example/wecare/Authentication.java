e18914667caa52a2f0ec0f524ff21a351fc9655d
MayurShelar <mayurshelar2002@gmail.com>
2023-03-17 14:10:57 +0530

    Testing commit

--- app/src/main/java/com/example/wecare/Authentication.java
+++ app/src/main/java/com/example/wecare/Authentication.java
@@ -10,5 +10,7 @@ public class Authentication extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+
+
     }
 }
